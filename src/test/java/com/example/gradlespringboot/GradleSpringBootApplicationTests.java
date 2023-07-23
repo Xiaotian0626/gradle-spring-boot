91636c819c863bf973c5a98845c24c473406d09e
Michael Lee <lixiaotiane@outlook.com>
2023-07-23 08:01:24 +0800

    Initial commit

---
+++ src/test/java/com/example/gradlespringboot/GradleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
